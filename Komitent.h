fdbbe2ec8ce6931d17eb57bc92e76e4e8c2b6a3c
Petar Perić <petarperic93@gmail.com>
2015-04-26 14:25:33 +0200

    Ubacene klasa za komitent-e i ubacene ikone

---
+++ Komitent.h
@@ -0,0 +1,19 @@
+#ifndef KOMITENT_H
+#define KOMITENT_H
+
+#include QString;
+
+class Komitent{
+private:
+    QString naziv;
+    int sifra;
+    QString ulica;
+    QString mesto;
+    unsigned int pib;
+    QString tekuciRacun;
+public:
+    Komitent();
+    Komitent(QString naziv, int sifra, QString ulica, QString mesto, unsigned int pib, QString tekuciRacun);
+};
+
+#endif // KOMITENT_H
