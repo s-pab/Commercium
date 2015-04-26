1a6d07c87b8acb80546721a488c7b6f2d8b33f0b
Petar Perić <petarperic93@gmail.com>
2015-04-26 20:19:13 +0200

    izmenjene faktura i profaktura

--- faktura.h
+++ faktura.h
@@ -1,4 +1,36 @@
 #ifndef FAKTURA_H
 #define FAKTURA_H
 
+#include "artikl.h"
+#include "komitent.h"
+#include <QDate>
+#include <QString>
+
+class Faktura{
+private:
+    int sifra;
+    QDate datum;
+    QString brOtpremnice;
+    int valuta;
+    Artikl artikl;
+    Komitent komintent;
+    int brArtikala;
+    double ukupnoNabavnaVrednost;
+    double ukupnoBezPVD;
+    double ukupnoPDV;
+    double ukupnoSaPDV;
+public:
+    Faktura();
+    Faktura(int sifra, Komitent komintent, QDate datum, QString brOtpremnice, int valuta);
+    void dodajArtikl(Artikl artikl);
+    void obrisiArtikl();
+    void izmeniArtikl();
+    void obracunaj();
+    Artikl getArtikl();
+    double getUkupnoNabavnaVrednost();
+    double getUkupnoBezPVD();
+    double getUkupnoPDV();
+    double getUkupnoSaPDV();
+};
+
 #endif // FAKTURA_H
--- profaktura.h
+++ profaktura.h
@@ -1,4 +1,36 @@
 #ifndef PROFAKTURA_H
 #define PROFAKTURA_H
 
+#include "artikl.h"
+#include "komitent.h"
+#include <QDate>
+#include <QString>
+
+class Profaktura{
+private:
+    int sifra;
+    QDate datum;
+    QString brOtpremnice;
+    int valuta;
+    Artikl artikl;
+    Komitent komintent;
+    int brArtikala;
+    double ukupnoNabavnaVrednost;
+    double ukupnoBezPVD;
+    double ukupnoPDV;
+    double ukupnoSaPDV;
+public:
+    Profaktura();
+    Profaktura(int sifra, Komitent komintent, QDate datum, QString brOtpremnice, int valuta);
+    void dodajArtikl(Artikl artikl);
+    void obrisiArtikl();
+    void izmeniArtikl();
+    void obracunaj();
+    Artikl getArtikl();
+    double getUkupnoNabavnaVrednost();
+    double getUkupnoBezPVD();
+    double getUkupnoPDV();
+    double getUkupnoSaPDV();
+};
+
 #endif // PROFAKTURA_H
