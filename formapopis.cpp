0376a3f731a043248225f7098a34b20a8b983ace
Petar Perić <petarperic93@gmail.com>
2015-05-25 01:26:23 +0200

    skoro pa zavrsen popis

--- formapopis.cpp
+++ formapopis.cpp
@@ -133,6 +133,7 @@ void FormaPopis::on_pushButton_2_clicked()
         ui->tableView_6->setModel(model);
         ui->tableView_6->resizeColumnsToContents();
         ui->tableView_6->resizeRowsToContents();
+        suma();
     }
 
 }
@@ -150,4 +151,93 @@ void FormaPopis::osvezi(int sif)
     ui->tableView_6->setModel(model);
     ui->tableView_6->resizeColumnsToContents();
     ui->tableView_6->resizeRowsToContents();
+}
+
+void FormaPopis::on_lineEdit_30_textChanged(const QString &arg1)
+{
+    QSqlQuery query;
+    query.prepare("SELECT SifraProizvoda,NazivProizvoda,ProdajnaCena,Kolicina,Ukupno FROM PopisPodaci WHERE brpopisa = ? AND NazivProizvoda LIKE '%'+?+'%'");
+    query.addBindValue(ui->spinBox->value());
+    query.addBindValue(arg1);
+    query.exec();
+    QSqlQueryModel* model = new QSqlQueryModel(this);
+    model->setQuery(query);
+    ui->tableView_6->setModel(model);
+    ui->tableView_6->resizeColumnsToContents();
+    ui->tableView_6->resizeRowsToContents();
+}
+
+void FormaPopis::on_tableView_6_doubleClicked(const QModelIndex &index)
+{
+    ui->lineEdit_29->setFocus();
+    int row = index.row();
+    ui->lineEdit_29->setText(index.sibling(row, 0).data().toString());
+
+    on_lineEdit_29_returnPressed();
+}
+
+void FormaPopis::suma()
+{
+    QSqlQuery query;
+    query.prepare("SELECT Ukupno FROM PopisPodaci WHERE brpopisa = :brpopisa");
+    query.bindValue(":brpopisa",ui->spinBox->value());
+    query.exec();
+    double suma = 0;
+    while(query.next())
+    {
+        suma+=query.value(0).toDouble();
+    }
+    QString pomS=QString::number(suma,'.',2);
+    pomS.replace('.',',');
+    ui->label_40->setText(pomS);
+}
+
+void FormaPopis::on_lineEdit_26_returnPressed()
+{
+    QSqlQuery query;
+    query.prepare("UPDATE PopisPodaci SET Kolicina=:kol, Ukupno=:sum WHERE SifraProizvoda=:id AND brpopisa = :brpopisa");
+    query.bindValue(":kol",ui->lineEdit_26->text().replace('.',','));
+    double pom=ui->lineEdit_26->text().toDouble()*ui->lineEdit_27->text().toDouble();
+    QString pomS=QString::number(pom,'.',2);
+    pomS.replace('.',',');
+    ui->lineEdit_28->setText(pomS);
+    query.bindValue(":sum",ui->lineEdit_28->text());
+    pomS.replace(',','.');
+    ui->lineEdit_28->setText(pomS);
+    query.bindValue(":id",ui->lineEdit_29->text().toInt());
+    query.bindValue(":brpopisa",ui->spinBox->value());
+    if(query.exec()&&kol!=ui->lineEdit_26->text())
+    {
+        ui->listWidget_6->addItem(ui->lineEdit_29->text()+". "+ui->lineEdit_30->text()+" Količina:"+kol+"->"+ui->lineEdit_26->text());
+        kol=ui->lineEdit_26->text();
+    }
+    QKeyEvent *event = new QKeyEvent ( QEvent::KeyPress, Qt::Key_Tab, Qt::NoModifier);
+    QCoreApplication::postEvent (this, event);
+    suma();
+    osvezi(ui->lineEdit_29->text().toInt());
+}
+
+void FormaPopis::on_lineEdit_27_returnPressed()
+{
+    if(ui->lineEdit_27->isModified())
+    {
+        QSqlQuery query;
+        query.prepare("UPDATE PopisPodaci SET ProdajnaCena=:cena WHERE Sifra=:id AND BrPopisa=:brpopisa");
+        QString pomS=QString::number(ui->lineEdit_27->text().toDouble(),'.',2);
+        pomS.replace('.',',');
+        query.bindValue(":cena",pomS);
+        query.bindValue(":id",ui->lineEdit_29->text().toInt());
+        query.bindValue(":brpopisa",ui->spinBox->value());
+        if(query.exec())
+        {
+            ui->listWidget_6->addItem(ui->lineEdit_29->text()+". "+ui->lineEdit_30->text()+" Cena:"+cena+"->"+ui->lineEdit_27->text());
+            cena=ui->lineEdit_27->text();
+        }
+        on_lineEdit_26_returnPressed();
+    }
+    else
+    {
+        QKeyEvent *event = new QKeyEvent ( QEvent::KeyPress, Qt::Key_Tab, Qt::NoModifier);
+        QCoreApplication::postEvent (this, event);
+    }
 }
--- formapopis.h
+++ formapopis.h
@@ -16,13 +16,21 @@ public:
     ~FormaPopis();
 
 private slots:
-    //void suma();
+    void suma();
     void osvezi(int sif);
     void on_pushButton_clicked();
 
     void on_lineEdit_29_returnPressed();
 
     void on_pushButton_2_clicked();
+
+    void on_lineEdit_30_textChanged(const QString &arg1);
+
+    void on_tableView_6_doubleClicked(const QModelIndex &index);
+
+    void on_lineEdit_26_returnPressed();
+
+    void on_lineEdit_27_returnPressed();
 
 private:
     Ui::FormaPopis *ui;
