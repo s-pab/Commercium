e36333e0e21e06d7bf80e79ff490ffb6c4b0ad3b
Petar Perić <petarperic93@gmail.com>
2015-06-17 10:32:28 +0200

    Popunjen artikli.cpp
    napsne su neke funkcije sad samo jos treba da se radi sa bazom

--- artikl.cpp
+++ artikl.cpp
@@ -13,68 +13,75 @@ Artikl::Artikl(int sifra,QString naziv,QString jedinica,int kategorija)
 }
 void Artikl::dodajCenu(double nabavna, double marza, double rabat)
 {
-
+    int pdv = 20; //treba da se promeni da cita iz bazu ali u bazu nemamo to za pdv
+    //treba da se promene lokalne promenjljive
+    this->prodajnaCena = (nabavna - rabat)*(1+pdv/100)+marza;
+    //treba da se promeni u bazu
 }
 void Artikl::dodajCenu(double nabavna, double prodajna)
 {
-
+    int pdv = 20;
+    this->marza = prodajna - nabavna*(1+pdv/100);
+    //treba da se promene lokalne promeljive i da se sve to doda u bazu
 }
 void Artikl::dodajCenuProcenat(double nabavna, double marzaP, double rabatP)
 {
-
+    int pdv = 20;
+    this->prodajnaCena = (nabavna*(1-rabat/100)*(1+pdv/100))*(1+marza/100);
+    //treba da se promene lokalne promenljivi i da se upise u bazu
 }
 void Artikl::setSifraProizvoda(int sifraProizvoda)
 {
-
+    this->sifraProizvoda = sifraProizvoda;
 }
 void Artikl::setNazivProizvoda(QString nazivProizvoda)
 {
-
+    this->nazivProizvoda = nazivProizvoda;
 }
 
 void Artikl::setProdajnaCena(double prodajnaCena)
 {
-
+    this->prodajnaCena = prodajnaCena;
 }
 
 void Artikl::setNabavnaCena(double nabavnaCena)
 {
-
+    this->nabavnaCena = nabavnaCena;
 }
 
 void Artikl::setMarza(double marza)
 {
-
+    this->marza = marza;
 }
 
 void Artikl::setMarzaProcenat(double marzaProcenat)
 {
-
+    this->marzaProcenat = marzaProcenat;
 }
 
 void Artikl::setRabat(double rabat)
 {
-
+    this->rabat = rabat;
 }
 
 void Artikl::setRabatProcenat(double rabatProcenat)
 {
-
+    this->rabatProcenat = rabatProcenat;
 }
 
 void Artikl::setJedinicaMere(QString jedinicaMere)
 {
-
+    this->jedinicaMere = jedinicaMere;
 }
 
 void Artikl::setKategorijaPDV(int kategorijaPDV)
 {
-
+    this->kategorijaPDV = kategorijaPDV;
 }
 
 void Artikl::setKolicina(int kolicina)
 {
-
+    this->kolicina = kolicina;
 }
 
 int Artikl::getSifraProizvoda()
