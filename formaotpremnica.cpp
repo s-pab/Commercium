53da0024196738a884bc6ce2c92e19eb42100d9a
sirko93 <stefan.sirko@gmail.com>
2015-06-22 18:14:39 +0200

    otpremnice

--- formaotpremnica.cpp
+++ formaotpremnica.cpp
@@ -44,10 +44,10 @@ void FormaOtpremnica::pretraga(QString naziv)
 {
     QSqlQuery query;
     if(naziv == "")
-        query.prepare("SELECT sifraProizvoda,nazivProizvoda,nabavnaCena,kolicina,jedinicaMere FROM Artikl");
+        query.prepare("SELECT sifraProizvoda,nazivProizvoda,nabavnaCena,jedinicaMere,kolicina FROM Artikl");
     else
     {
-        query.prepare("SELECT sifraProizvoda,nazivProizvoda,prodajnaCena,kolicina,jedinicaMere FROM Artikl WHERE nazivProizvoda LIKE '%'+?+'%'");
+        query.prepare("SELECT sifraProizvoda,nazivProizvoda,prodajnaCena,jedinicaMere,kolicina FROM Artikl WHERE nazivProizvoda LIKE '%'+?+'%'");
         query.addBindValue(naziv);
     }
     query.exec();
@@ -114,7 +114,7 @@ void FormaOtpremnica::on_nova_clicked()
     else
     {
         query.prepare("INSERT INTO OtpremnicaOsnovno (sifraOtpremnica,datum,komitent,ukupnaVrednost) VALUES (:SIFRA,:DATUM,:KOMITENT,:UKUPNAVREDNOST)");
-        query.bindValue(":SIFRA",ui->sifra->text().toInt());
+        query.bindValue(":SIFRA",ui->brojOtpremnice->text().toInt());
         QDate Datum = ui->datum->date();
         query.bindValue(":DATUM",Datum);
         query.bindValue(":KOMITENT",ui->komitent->text().toInt());
@@ -142,7 +142,7 @@ void FormaOtpremnica::on_ubaciArtikl_clicked()
     query2.bindValue(":SIFRA",index.sibling(row,0).data().toInt());
     query2.bindValue(":NAZIV",index.sibling(row,1).data().toString());
     query2.bindValue(":CENA",index.sibling(row,2).data().toInt());
-    query2.bindValue(":JEDINICAMERE",index.sibling(row,4).data().toString());
+    query2.bindValue(":JEDINICAMERE",index.sibling(row,3).data().toString());
     query2.bindValue(":KOLICINA",index.sibling(row,4).data().toInt());
     query2.bindValue(":brOtpremnice",ui->brojOtpremnice->text().toInt());
     query2.exec();
@@ -154,7 +154,7 @@ void FormaOtpremnica::on_izmeni_clicked()
     QSqlQuery query;
     QSqlQuery query2;
     bool postoji = false;
-    query.exec("SELECT sifraOtpremnice FROM OtpremniceOsnovno");
+    query.exec("SELECT brOtpremnice FROM OtpremnicaOsnovno");
     if(ui->brojOtpremnice->text()!="")
     while(query.next())
     {
@@ -176,7 +176,7 @@ void FormaOtpremnica::on_izmeni_clicked()
     }
     else
     {
-        query.prepare("SELECT * FROM OtpremnicaOsnovno WHERE sifraOtpremnice=:SIFRA");
+        query.prepare("SELECT * FROM OtpremnicaOsnovno WHERE brOtpremnice=:SIFRA");
         query.bindValue(":SIFRA",ui->brojOtpremnice->text().toInt());
         query.exec();
         if(query.next())
